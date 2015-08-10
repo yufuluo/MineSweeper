0e2010041585fe14eaab1a269931fe35857d445d
Kay Lau <qijunliu7@gmail.com>
2015-08-10 01:41:43 -0700

    click impl

--- src/MineSweeper/MineSweeper.java
+++ src/MineSweeper/MineSweeper.java
@@ -6,6 +6,28 @@ public class MineSweeper {
 	private boolean[][] shown;
 	
 	public Status click(int x, int y) {
-		return Status.CONTINUE;
+		if (shown[x][y] || x>=mine[0].length || y>=mine.length || x<0 || y<0) {
+			return Status.CONTINUE;
+		}
+		if (mine[x][y] == MINE) {
+			return Status.LOSS;
+		}
+		if (mine[x][y] != 0) {
+			shown[x][y] = true;
+			return Status.CONTINUE;
+		} else {
+			shown[x][y] = true;
+			click(x-1,y+1);
+			click(x-1,y);
+			click(x-1,y-1);
+			click(x,y+1);
+			click(x,y-1);
+			click(x+1,y+1);
+			click(x+1,y);
+			click(x+1,y-1);
+			return Status.CONTINUE;
+		}
 	}
+	
 }
+
